35e9f8f091f50ffe126674d46e984ec85930a910
Mateus Martins Pinheiro <mateusmartinsmck@gmail.com>
2023-10-06 21:10:54 -0300

    fix: bg-emphasis

--- components/product/DiscountBadge.tsx
+++ components/product/DiscountBadge.tsx
@@ -14,7 +14,7 @@ function DiscountBadge({ price, listPrice, label, variant, className }: Props) {
       class={`absolute left-0 top-0 lg:p-[10px] flex items-center z-10 ${className}`}
     >
       <div
-        class={`text-xs uppercase font-bold border-none px-[10px] py-[7px] rounded-lg flex box-content bg-opacity-100 opacity-100 text-primary bg-${
+        class={`text-xs uppercase font-bold border-none px-[10px] py-[7px] rounded-lg flex box-content bg-opacity-100 opacity-100 text-white bg-emphasis bg-${
           variant ?? "secondary"
         }`}
       >
